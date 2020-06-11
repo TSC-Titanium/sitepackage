1384253397d6eada68dba88d90ee1601a18885a6
Friedrich Rehren <development@friedrichrehren.de>
2020-06-11 13:17:13 +0200

    working on nav dropdown level 4

--- Resources/Public/JavaScript/Dist/scripts.js
+++ Resources/Public/JavaScript/Dist/scripts.js
@@ -3,4 +3,18 @@
  * Copyright 2017-2020 Friedrich Rehren
  * Licensed under the GPL-2.0-or-later license
  */
-console.log("WE LOVE TYPO3");
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+    if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+    }
+    var $subMenu = $(this).next('.dropdown-menu');
+    $subMenu.toggleClass('show');
+  
+  
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+      $('.dropdown-submenu .show').removeClass('show');
+    });
+  
+  
+    return false;
+  });
